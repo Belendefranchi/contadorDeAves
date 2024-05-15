a1df53a1e16cbefbd96201f53d54c8d38538d74d
Belén De Franchi <belendefranchi@gmail.com>
2024-05-15 16:24:47 -0300

    hago que el valor del contador 1 se guarde dividido por 2, para regular el sensor a la altura de las patas

--- iot/iot.php
+++ iot/iot.php
@@ -5,7 +5,11 @@
     // Leemos los valores que nos llegan por GET
    $valor = $_GET['valor'];
    $contador = $_GET['contador'];
-    
+    //contador 1, cuenta patas
+   if ($contador = 1){
+        $valor = Math.ceil($valor / 2);  
+   };
+
     // Esta es la instrucción para insertar los valores
     $query = "INSERT INTO valores(valor, ID) VALUES('$valor', '$contador')";
     // Ejecutamos la instrucción
